bdbd80a66c50125ca6fe97405e2826433100ad69
AleJhunior10 <alejhunior10@gmail.com>
2020-09-24 23:17:05 -0400

    UPDATE JavaSript 2.0

--- Proyecto/ProyectoFF/JavaScript/JsMenuInicio.js
+++ Proyecto/ProyectoFF/JavaScript/JsMenuInicio.js
@@ -18,10 +18,20 @@ var hora = ((h.getHours() < 10) ? "0" : "") + h.getHours();
 var min = ((h.getMinutes() < 10) ? "0" : "") + h.getMinutes();
 var seg = ((h.getSeconds() < 10) ? "0" : "") + h.getSeconds();
 
-var dia=h.getDay();
+/*var dia=h.getDay();
 var mes=h.getMonth()+1;
-var anio=h.getFullYear();
+var anio=h.getFullYear();*/
 
-document.write("Hora Actual : " + hora + " : " + min + " : " + seg + " de " + dia + " / " + mes + " / " + anio);
+document.write(hora + " : " + min + " : " + seg /*+ " de " + dia + " / " + mes + " / " + anio*/);
 
+/*
+var hoy = new Date();
+var numDia = hoy.getUTCDate() - 1;
+var mes = hoy.getUTCMonth() + 1;
+var anio = hoy.getUTCFullYear();
+var dias = ['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
+var numeroDiaSemana = hoy.getDay();
 
+document.write("Hora : " + hora + " : " + min + " : " + seg + " Fecha : " +  dias[numeroDiaSemana] + " " + numDia +" / "+ mes + " / " + anio);
+document.write();
+*/
