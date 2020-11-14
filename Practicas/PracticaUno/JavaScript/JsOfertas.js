2899eefd733ac69334c85f1cdeb00f0661788927
AleJhunior10 <alejhunior10@gmail.com>
2020-11-13 23:58:34 -0400

    Update PracticaUno

---
+++ Practicas/PracticaUno/JavaScript/JsOfertas.js
@@ -0,0 +1,9 @@
+Swal.fire({
+    title: 'Ofertas!',
+    background: 'white',
+    text: 'Combos y Promociones.',
+    imageUrl: 'https://image.freepik.com/vector-gratis/plantilla-banners-comidas-combinadas_23-2148674417.jpg',
+    imageWidth: 500,
+    imageHeight: 400,
+    imageAlt: 'Custom image',
+  })
